64ba6da09d66b6b8cd4c62630e0a7fe6d8e180aa
Rogier Grobbee <r.grobbee@gmail.com>
2016-10-21 15:15:51 +0200

    plane edits

--- src/main/java/nl/han/ica/Planesgame/Plane.java
+++ src/main/java/nl/han/ica/Planesgame/Plane.java
@@ -1,6 +1,8 @@
 package nl.han.ica.Planesgame;
 
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import processing.core.PApplet;
+import processing.core.PGraphics;
 
 public class Plane extends HittableMovingObject implements ICanShootBullets {
 
@@ -10,12 +12,12 @@ public class Plane extends HittableMovingObject implements ICanShootBullets {
     private float ay;
     private float vx;
     private float vy;
-    private int rotation;
     private int playerNumber;
     private int score;
     boolean rotateRight;
     boolean rotateLeft;
     boolean thrustInOn;
+    private float rotatiehoek = 0;
 
     public Plane(PlanesApp app) {
         super(new Sprite("src/main/java/nl/han/ica/Planesgame/resources/planesprite1.png"));
@@ -27,8 +29,53 @@ public class Plane extends HittableMovingObject implements ICanShootBullets {
 
     }
 
-    public void objectWasHitByBullet(ICanShootBullets shooter) {
+    @Override
+    public void draw(PGraphics g) {
 
+        g.pushMatrix();
+        g.translate(getCenterX(), getCenterY());
+
+        g.rotate(PApplet.radians(rotatiehoek));
+
+        g.image(getImage(), -width / 2, -height / 2);
+        g.popMatrix();
+
+    }
+
+    public static float getRotationInRadians(float rotationInDegrees) {
+        float rotationInRadians = (float) (PApplet.radians(rotationInDegrees) % Math.PI);
+        rotationInRadians = (float) (((rotationInRadians > Math.PI * 0.5 && rotationInRadians < Math.PI * 1) || (rotationInRadians > Math.PI * 1.5 && rotationInRadians < Math.PI * 2)) ? Math.PI - rotationInRadians : rotationInRadians);
+
+        return rotationInRadians;
+    }
+
+    @Override
+    public float getWidth() {
+        // met dank aan
+        // http://stackoverflow.com/questions/10392658/calculate-the-bounding-boxs-x-y-height-and-width-of-a-rotated-element-via-jav
+        float rotationInRadians = getRotationInRadians(rotatiehoek);
+        return (float) (Math.sin(rotationInRadians) * height + Math.cos(rotationInRadians) * width);
+    }
+
+    @Override
+    public float getHeight() {
+        float rotationInRadians = getRotationInRadians(rotatiehoek);
+        return (float) (Math.sin(rotationInRadians) * width + Math.cos(rotationInRadians) * height);
+    }
+
+    @Override
+    public float getX() {
+        return -(getWidth() / 2) + getCenterX();
+    }
+
+    @Override
+    public float getY() {
+        return -(getHeight() / 2) + getCenterY();
+    }
+
+    @Override
+    public void objectWasHitByBullet(ICanShootBullets shooter) {
+        System.out.println("Outch");
     }
 
     public void shoot() {
@@ -111,7 +158,56 @@ public class Plane extends HittableMovingObject implements ICanShootBullets {
 
     public void update() {
         if (thrustInOn) {
-            x = x + 1;
+
+            float beweeg = 3;
+
+            if (rotatiehoek >= 0 && rotatiehoek < 90){
+                float gradeY = rotatiehoek;
+                float gradeX = Math.abs(rotatiehoek - 90);
+
+                x = x + ((beweeg / 90) * gradeX);
+                y = y + ((beweeg / 90) * gradeY);
+            }
+            else if (rotatiehoek >= 90 && rotatiehoek < 180){
+                float rotatie = rotatiehoek - 90;
+                float gradeY = rotatie;
+                float gradeX = Math.abs(rotatie - 90);
+
+                x = x - ((beweeg / 90) * gradeY);
+                y = y + ((beweeg / 90) * gradeX);
+            }
+            else if (rotatiehoek >= 180 && rotatiehoek < 270){
+                float rotatie = rotatiehoek - 180;
+                float gradeY = rotatie;
+                float gradeX = Math.abs(rotatie - 90);
+
+                x = x - ((beweeg / 90) * gradeX);
+                y = y - ((beweeg / 90) * gradeY);
+            }
+            else if (rotatiehoek >= 270 && rotatiehoek <= 359){
+                float rotatie = rotatiehoek - 270;
+                float gradeY = rotatie;
+                float gradeX = Math.abs(rotatie - 90);
+
+                x = x + ((beweeg / 90) * gradeY);
+                y = y - ((beweeg / 90) * gradeX);
+            }
+        }
+        if (rotateLeft) {
+            if ((rotatiehoek - 1) < 0){
+                rotatiehoek = 359;
+            }
+            else {
+                rotatiehoek = rotatiehoek - 2;
+            }
+        }
+        if (rotateRight) {
+            if ((rotatiehoek + 1) > 359){
+                rotatiehoek = 0;
+            }
+            else {
+                rotatiehoek = rotatiehoek + 2;
+            }
         }
     }
 
